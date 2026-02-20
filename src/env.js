fba028de2615e336b4f22eb405772a9575d34d17
Stefan Jakovljevic <stefanjakovljevic.dev@gmail.com>
2026-02-20 02:54:31 -0500

    added read db

--- src/env.js
+++ src/env.js
@@ -11,6 +11,8 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    DIRECT_URL: z.string().url(),
+
   },
 
   /**
@@ -28,6 +30,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    DIRECT_URL: process.env.DIRECT_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
