fc4f2e295ef3a44be000c524853e408a9e0f9bd6
Stefan Jakovljevic <stefanjakovljevic.dev@gmail.com>
2026-02-19 20:33:31 -0500

    added color scheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${comfortaa.variable}`}>
-      <body className="font-sans">
+      <body className="font-sans bg-brand-primary text-white">
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
 import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
   return (
     <HydrateClient>
-      <main className="p-6">
-        <h1 className="text-2xl font-semibold">flashcards.io</h1>
+      <main className="min-h-screen flex flex-col gap-2 items-center justify-center">
+        <h1 className="text-4xl font-semibold text-center">flashcards.io</h1>
+        <Button className="bg-brand-primary border-2 border-brand-secondary text-lg hover:bg-brand-secondary hover:text-brand-primary">
+          <Link href="/learn">Start learning</Link>
+        </Button>
       </main>
     </HydrateClient>
   );
